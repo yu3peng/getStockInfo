8ed9139bfdce98b885a1d6bca9ac58695ac5cf34
yupeng <yu3peng@qq.com>
2020-10-28 15:59:20 +0800

    Update getStockInfoFrom163.py

--- getStockInfoFrom163.py
+++ getStockInfoFrom163.py
@@ -3,25 +3,24 @@ import re
 import urllib3
 import os
 import time
+import re     #引入正则表达式库，便于后续提取股票代码
 
 # 获取股票代码列表
-def urlTolist(url):
-    allCodeList = []
-    html = urllib.request.urlopen(url).read()
-    html = html.decode('gbk', 'ignore')
-    s = r'<li><a target="_blank" href="http://quote.eastmoney.com/\S\S(.*?).html">'
-    pat = re.compile(s)
-    code = pat.findall(html)
-    for item in code:
-        if item[0] == '6' or item[0] == '3' or item[0] == '0':
-            allCodeList.append(item)
-    return allCodeList
-
+def getStockList(lst, stockURL):          #获取股票代码列表
+    html = getHTMLText(stockURL, "GB2312")
+    soup = BeautifulSoup(html, 'html.parser') 
+    a = soup.find_all('a')      #得到一个列表
+    for i in a:
+        try:
+            href = i.attrs['href']       #股票代码都存放在href标签中
+            lst.append(re.findall(r"[S][HZ]\d{6}", href)[0])
+        except:
+            continue
 
 def main():
-    
-    stock_nos = urlTolist('http://quote.eastmoney.com/stocklist.html')
-
+    stock_list_url = 'https://hq.gucheng.com/gpdmylb.html'
+    stock_nos=[]
+    getStockList(stock_nos, stock_list_url)    
     for stock_no in stock_nos:
         print("stock_no:" + stock_no + "\n")
         zycwzb = 'http://quotes.money.163.com/service/zycwzb_'+ stock_no +'.html?type=report'
