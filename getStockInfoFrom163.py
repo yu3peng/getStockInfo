31a5d78896be6658956ec49989345f207ff67e8b
yupeng <yu3peng@qq.com>
2020-10-29 15:27:43 +0800

    Update getStockInfoFrom163.py

--- getStockInfoFrom163.py
+++ getStockInfoFrom163.py
@@ -69,5 +69,5 @@ def main():
     getStockList(stockList)
     getAllStockInfo(stockList)
     time_cost = time.perf_counter() - start
-    print("爬取成功，共用时：" + time_cost)
+    print("爬取成功,共用时：{:.2f}s".format(time_cost))
 main()
