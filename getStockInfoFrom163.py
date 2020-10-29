ecade45f1346c327cacb67dfd1c21298a4b3a01a
yupeng <yu3peng@qq.com>
2020-10-29 13:55:57 +0800

    Update getStockInfoFrom163.py

--- getStockInfoFrom163.py
+++ getStockInfoFrom163.py
@@ -5,9 +5,9 @@ import re
 import urllib3
 import os
 import time
-import re
 
-def getHTMLText(url, code="utf-8"):  #获取HTML文本
+# 获取HTML文本
+def getHTMLText(url, code="utf-8"):  
     try:
         r = requests.get(url)
         r.raise_for_status()
@@ -17,34 +17,36 @@ def getHTMLText(url, code="utf-8"):  #获取HTML文本
         return ""
 
 # 获取股票代码列表
-def getStockList(lst, stockURL):          #获取股票代码列表
-    html = getHTMLText(stockURL, "GB2312")
+def getStockList(stockList):
+    html = getHTMLText('https://hq.gucheng.com/gpdmylb.html', "GB2312")
     soup = BeautifulSoup(html, 'html.parser')
-    a = soup.find_all('a')      #得到一个列表
+    # 得到一个列表
+    a = soup.find_all('a')
     for i in a:
         try:
-            href = i.attrs['href']       #股票代码都存放在href标签中
-            lst.append("0" + re.findall(r"[S][HZ]\d{6}", href)[0][2:8])
+            # 股票代码都存放在href标签中
+            href = i.attrs['href']
+            # re.findall(r"[S][HZ]\d{6}", href)[0]，从gucheng网获取到的股票代码为SH000001格式，需要转换为163上需要的格式0000001
+            stockList.append("0" + re.findall(r"[S][HZ]\d{6}", href)[0][2:8])
         except:
             continue       
             
 def main():
-    stock_list_url = 'https://hq.gucheng.com/gpdmylb.html'
-    stock_nos=[]
-    stock_nos_qc=[]
-    getStockList(stock_nos, stock_list_url)
-    stock_nos_qc=list(set(stock_nos))
-    for stock_no in stock_nos_qc:
-        code = 'http://quotes.money.163.com/service/chddata.html?code=' + stock_no + '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
+    stockList=[]
+    # 去重使用
+    stockListQC=[]
+    getStockList(stockList)
+    stockListQC=list(set(stockList))
+    for stockNO in stockListQC:
+        # TCLOSE收盘价 ;HIGH最高价;LOW最低价;TOPEN开盘价;LCLOSE前收盘价;CHG涨跌额;PCHG涨跌幅;TURNOVER换手率;VOTURNOVER成交量;VATURNOVER成交金额;TCAP总市值;MCAP流通市值
+        code = 'http://quotes.money.163.com/service/chddata.html?code=' + stockNO + '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
         http = urllib3.PoolManager()
-        if not os.path.exists(stock_no):
-            os.makedirs('stocks/'+stock_no)
-
+        if not os.path.exists(stockNO):
+            os.makedirs('stocks/'+stockNO)
         response = http.request('GET',code)
-        with open('stocks/'+ stock_no + '/code.csv', 'wb') as f:
+        with open('stocks/'+ stockNO + '/code.csv', 'wb') as f:
             f.write(response.data)
         response.release_conn()
-        print('完成'+stock_no)
-
+        print('完成'+stockNO)
 
 main()
